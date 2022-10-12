e0f752db4d0b1d0fdbffd748d831b7e6645bc557
nathanjpak <nathan.j.pak@gmail.com>
2022-10-12 15:01:31 -0700

    Install MathJax library

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import MathJax from "@innodoc/react-mathjax-node";
 
 const App = () => {
   return (
-    <div>
-      Hello!
-    </div>
+    <MathJax.Provider>
+      <p>
+        Inline Math:{' '}
+        <MathJax.MathJaxNode displayType="inline" texCode="f(x)=x^2+b_1" />
+      </p>
+    </MathJax.Provider>
   );
 }
 
